efe1f3c48528b275aae7b2e0a0c1d355ead7be99
edo6661 <muhammadridho5654@gmail.com>
2025-03-02 03:54:59 +0700

    wip: projects page

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,6 +1,8 @@
 "use client"
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
+import GithubCorner from './GithubCorner'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export type Tech = "React" | "Vue" | "Flutter" | "Kotlin" | "Typescript" | "NextJS"
 const techs: Tech[] = ["React", "Vue", "Flutter", "Kotlin", "Typescript", "NextJS"]
@@ -47,7 +49,23 @@ export type Project = {
 }
 const initialProjects: Project[] = [
   {
-    image: "images/projects/nike.png",
+    image: "/images/projects/nike.png",
+    title: "Nike Clone",
+    tech: ["React", "Typescript", "NextJS"],
+    description: "This is a project description",
+    url: "https://nike-clone-static.vercel.app/",
+    githubUrl: "https://github.com/edo6661/nike-clone-static",
+  },
+  {
+    image: "/images/projects/nike.png",
+    title: "Nike Clone",
+    tech: ["React", "Typescript", "NextJS"],
+    description: "This is a project description",
+    url: "https://nike-clone-static.vercel.app/",
+    githubUrl: "https://github.com/edo6661/nike-clone-static",
+  },
+  {
+    image: "/images/projects/nike.png",
     title: "Nike Clone",
     tech: ["React", "Typescript", "NextJS"],
     description: "This is a project description",
@@ -56,12 +74,12 @@ const initialProjects: Project[] = [
   },
 ];
 
-const initialTechs: Tech[] = ["React", "Vue", "Flutter", "Kotlin", "Typescript"]
+const initialTechs: Tech[] = ["React", "Vue", "Flutter", "Kotlin", "Typescript", "NextJS"]
 
 
 const Projects = () => {
   const [selectedTech, setSelectedTech] = useState<Tech[]>(initialTechs)
-  const [projects, setProjects] = useState<Project[]>(initialProjects)
+  const [projects] = useState<Project[]>(initialProjects)
 
   const onChangeTech = (tech: Tech) => {
     if (selectedTech.includes(tech)) {
@@ -70,26 +88,46 @@ const Projects = () => {
       setSelectedTech([...selectedTech, tech])
     }
   }
-  const filteredProjects = projects.filter((project) => {
-    return project.tech.some((tech) => selectedTech.includes(tech))
+  const filteredProjects = useMemo(() => {
+    return projects.filter((project) => {
+      return project.tech.some((tech) => selectedTech.includes(tech))
+    })
+  }, [projects, selectedTech])
+  const allSelected = useMemo(() => selectedTech.length === techs.length, [selectedTech])
+  const onAllSelected = () => {
+    if (allSelected) {
+      setSelectedTech([])
+    } else {
+      setSelectedTech(techs)
+    }
   }
-  )
+
 
   return (
     <div className='text-secondary flex md:flex-row flex-col md:flex-1 min-h-[620px] items-stretch'>
       <LeftSection
         selectedTech={selectedTech}
         onChangeTech={onChangeTech}
+        allSelected={allSelected}
+        onAllSelected={onAllSelected}
       />
-      <RightSection />
+      <RightSection
+        filteredProjects={filteredProjects}
+
+      />
     </div>
   )
 }
 
 const LeftSection = (
-  { selectedTech, onChangeTech }: {
+  { selectedTech, onChangeTech,
+    allSelected, onAllSelected
+
+  }: {
     selectedTech: Tech[]
-    onChangeTech: (tech: Tech) => void
+    onChangeTech: (tech: Tech) => void,
+    allSelected: boolean,
+    onAllSelected: () => void
   }
 ) => {
   const isActive = (tech: Tech) => selectedTech.includes(tech)
@@ -110,28 +148,36 @@ const LeftSection = (
           projects
         </span>
       </div>
-      <div className='space-y-4 px-4 py-2'>
-        {techs.map((tech) => (
-          <div key={tech} className='flex items-center gap-2'>
-            <div className="flex gap-4 items-center">
-              <div className="flex items-center gap-2">
-                <input type='checkbox'
-                  checked={isActive(tech)}
-                  onChange={() => onChangeTech(tech)}
-                />
-                <Image
-                  src={projectTech[tech].image}
-                  alt={projectTech[tech].title}
-                  width={20}
-                  height={20}
-                />
-              </div>
-              <span className={`text-sm ${isActive(tech) ? 'text-secondary-white' : 'text-secondary'}`}>
-                {projectTech[tech].title}
-              </span>
+      <div className='space-y-4 px-4 py-2 md:border-none border-b border-line'>
+        <div className='flex items-center gap-2'>
+          <div className="flex gap-4 items-center">
+            <div className="flex items-center gap-2">
+              <input type='checkbox'
+                checked={allSelected}
+                onChange={onAllSelected}
+                id='all'
+              />
+
 
             </div>
+            <label
+              htmlFor='all'
+              className={`text-sm ${allSelected ? 'text-secondary-white' : 'text-secondary'}`}>
+              All
+            </label>
+
           </div>
+        </div>
+        {techs.map((tech, i) => (
+          <TechCheckbox
+            key={i}
+            image={projectTech[tech].image}
+            isActive={isActive}
+            onChangeTech={onChangeTech}
+            tech={tech}
+
+          />
+
         ))}
       </div>
     </div>
@@ -139,17 +185,103 @@ const LeftSection = (
   )
 }
 
-const RightSection = () => {
+export const TechCheckbox = (
+  { image, tech, isActive, onChangeTech }: {
+    image: string,
+    tech: Tech,
+    isActive: (tech: Tech) => boolean,
+    onChangeTech: (tech: Tech) => void
+  }
+
+) => {
+  return <div className='flex items-center gap-2'>
+    <div className="flex gap-4 items-center">
+      <div className="flex items-center gap-2">
+        <input type='checkbox'
+          id={tech}
+          checked={isActive(tech)}
+          onChange={() => onChangeTech(tech)}
+        />
+        <Image
+          src={image}
+          alt={tech}
+          width={20}
+          height={20}
+        />
+      </div>
+      <label
+        htmlFor={tech}
+        className={`text-sm ${isActive(tech) ? 'text-secondary-white' : 'text-secondary'}`}>
+        {tech}
+      </label>
+
+    </div>
+  </div>
+
+
+}
+const RightSection = (
+  { filteredProjects }: {
+    filteredProjects: Project[]
+  }
+) => {
+  const isNotEmpty = filteredProjects.length > 0;
   return (
     <div className='flex flex-col flex-1 min-h-full '>
-      <div className='flex gap-2 items-center border-b border-line'>
+      <div className='md:flex hidden gap-2 items-center border-b border-line'>
         <span className='px-4 py-2'>
           all;
         </span>
       </div>
-      <div>
 
-      </div>
+      {isNotEmpty && (
+        <div className={`flex flex-wrap px-6 py-4 gap-8 ${!isNotEmpty ? 'flex-1 justify-center items-center' : 'justify-around'}`}
+        >
+          {filteredProjects.map((project, i) =>
+            <div key={i} className='border-line bg-primary-light border rounded-md relative'>
+              <GithubCorner
+                url={project.githubUrl}
+              />
+              <Image
+                src={project.image}
+                width={200}
+                height={200}
+                className='w-full object-cover rounded-md'
+                alt={project.title}
+              />
+              <div className='flex flex-col gap-4 py-6 px-4'>
+                <div className="flex flex-col gap-2">
+                  <span>
+                    {project.title}
+                  </span>
+                  <span className='text-xs font-thin'>
+                    {project.description}
+                  </span>
+                </div>
+                <a target='_blank' href={project.url}
+                  className='bg-[#1C2B3A] block text-secondary-white rounded-sm text-center py-2 px-4 hover:bg-[#263B50] transition-colors duration-300 w-fit'
+                >
+                  view-project
+                </a>
+
+              </div>
+
+
+            </div>
+          )}
+        </div>
+
+      )}
+
+      {!isNotEmpty && (
+        <div className='flex flex-col items-center justify-center flex-1 '
+
+        >
+          <span className='text-secondary'>
+            No projects found
+          </span>
+        </div>
+      )}
     </div>
 
   )
